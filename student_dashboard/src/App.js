477b27610a476fcbbdbee13983d46f7d115078c4
Barzin <barzin.development@gmail.com>
2023-01-31 12:28:38 +0100

    Student Dashboard => Add the AssignmentChart and StudentChart class components. Add these components to App.js. Also install react-router-dom package voor routing the pages.

--- student_dashboard/src/App.js
+++ student_dashboard/src/App.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from "react";
 // import OverviewChart from "./components/charts/OverviewChart";
-import Testing from './components/charts/Testing';
+// import Testing from './components/charts/Testing';
+
+import AssignmentChart from "./components/charts/AssignmentChart";
+import StudentChart from "./components/charts/StudentChart";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home</div>}/>
+          <Route path="student" element={<div>Student</div>}/>
+        </Routes>
+      </BrowserRouter>
       {/* <OverviewChart /> */}
-      <Testing />
+      {/* <Testing /> */}
+      <AssignmentChart />
+      <StudentChart />
     </div>
   );
 }
---
+++ student_dashboard/src/components/charts/AssignmentChart.js
@@ -0,0 +1,115 @@
+import React from "react";
+import data from "../../assets/data.json";
+import { VictoryChart, VictoryBar, VictoryGroup, VictoryAxis, VictoryTheme, VictoryContainer } from 'victory';
+
+let newData = []
+data.map(item => {return newData.push(item)})
+
+class AssignmentChart extends React.Component {
+    constructor(props) {
+      super(props) 
+      this.state = {
+        assignmentData: [],
+        difficulty: true,
+        enjoyment: true,
+      }
+    
+      this.handleChange = this.handleChange.bind(this)
+      this.filterAssignments = this.filterAssignments.bind(this)
+    }
+
+    handleChange(event) {
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? 
+            this.setState({[name]: checked}) : 
+            this.setState({[name]: value}) 
+    }
+
+    filterAssignments() {
+      let value = this.props.assignment 
+      let filteredItems = newData.filter((item) => {return item.assignment === value})
+      
+      this.setState((prevState) => {
+        let newState = {...prevState, assignmentData: filteredItems}
+        return newState; 
+    })
+  }
+
+    componentDidMount(){
+      this.filterAssignments()
+  }
+
+    componentDidUpdate(prevProps) {
+      if (this.props.assignment !== prevProps.assignment) {
+      this.filterAssignments(this.props.assignment)}
+  }
+   
+    render() {
+      return (
+        <div>
+          <label className="diff">
+            <input 
+              className="blue-input"
+              type="checkbox"
+              name="difficulty"
+              onChange={this.handleChange}
+              checked={this.state.difficulty}
+            /> 
+            Difficulty
+          </label>
+          <label className="enjoy">
+            <input 
+              className="orange-input"
+              type="checkbox"
+              name="enjoyment"
+              onChange={this.handleChange}
+              checked={this.state.enjoyment}
+            /> 
+            Enjoyment
+          </label>
+          
+          <VictoryChart
+            domainPadding={{x: 15}}
+            domain={{ x: [0, 10], y: [0.0, 5.0] }}
+            theme={VictoryTheme.material}
+            width={800} height={300}
+            containerComponent={<VictoryContainer responsive={false}/>}
+          >
+          <VictoryAxis 
+            style={{ ticks: {stroke: "grey", size: 5},
+            tickLabels: {fontSize: 16, padding: 5} }}
+          />
+          <VictoryAxis 
+            dependentAxis
+          />
+          <VictoryGroup offset={10}
+            colorScale={"qualitative"}
+          >
+
+          {this.state.difficulty ?   
+            <VictoryBar
+              style={{ data: { fill: "#4f8bc9" } }}
+              barWidth={10}  
+              data={this.state.assignmentData}
+              x={"name"}
+              y={"difficultyRating"}
+            /> 
+          : null}
+            
+          {this.state.enjoyment ?
+            <VictoryBar 
+              style={{ data: { fill: "#ffb212" } }}
+              barWidth={10}
+              data={this.state.assignmentData}
+              x={"name"}
+              y={"enjoymentRating"}
+            /> 
+          : null}
+                     
+          </VictoryGroup>
+          </VictoryChart>
+        </div> 
+    )}
+}    
+
+export default AssignmentChart
